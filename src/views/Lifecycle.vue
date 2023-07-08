c13fa0402f65439ca9f9e124820c04693224ba4f
Kai.ting.j <dear17530@gmail.com>
2023-07-08 21:43:06 +0800

    vue: watch / computed

--- src/views/Lifecycle.vue
+++ src/views/Lifecycle.vue
@@ -1,56 +1,58 @@
 <template>
   <div ref="refDiv">{{ text }}</div>
   <button @click="change">改字</button>
+
+  <div>{{ person.name }}</div>
+  <div>{{ person.num }}</div>
+  <div>computed: {{ rename }}</div>
+
+  <button @click="changeName">改名</button>
+  <button @click="() => person.num++">++</button>
 </template>
 
 <script setup>
-import {
-  ref,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted
-} from 'vue'
+import { ref, reactive, watch, computed } from 'vue'
 
 const refDiv = ref(null)
 const text = ref('div')
-
-console.log('setup(created)', refDiv.value)
+const person = reactive({
+  name: 'KT',
+  num: 1
+})
 
 const change = () => {
   console.log('click change')
   text.value = 'new div'
 }
 
-onBeforeMount(() => {
-  console.log('onBeforeMount', refDiv.value)
-})
+const changeName = () => {
+  console.log('click changeName')
+  person.name = 'KT2'
+}
 
-onMounted(() => {
-  console.log('onMounted', refDiv.value)
-})
+const rename = computed(() => person.name + person.num)
 
-onBeforeUpdate(() => {
-  console.log('onBeforeUpdate', refDiv.value.outerHTML)
-})
+watch(
+  () => text.value,
+  (nv, ov) => {
+    console.log('watch text nv', nv)
+    console.log('watch text ov', ov)
+  }
+)
 
-onUpdated(() => {
-  console.log('onUpdated', refDiv.value.outerHTML)
-})
+watch(
+  () => ({ ...person }),
+  (nv, ov) => {
+    console.log('watch person nv', nv)
+    console.log('watch person ov', ov)
+  }
+)
 
-onBeforeUnmount(() => {
-  console.log('onBeforeUnmount')
-})
-
-const count = ref()
-count.value = setInterval(() => {
-  console.log(1)
-}, 1000)
-
-onUnmounted(() => {
-  console.log('onUnmounted')
-  if (count.value) clearInterval(count.value)
-})
+watch(
+  () => rename.value,
+  (nv, ov) => {
+    console.log('watch rename nv', nv)
+    console.log('watch rename ov', ov)
+  }
+)
 </script>
