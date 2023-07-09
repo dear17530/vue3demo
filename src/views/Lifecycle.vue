c4baff0ee9e1ce6dcb643c56193778a1fa11bac0
Kai.ting.j <dear17530@gmail.com>
2023-07-09 20:03:58 +0800

    vue 語法: v-bind

--- src/views/Lifecycle.vue
+++ src/views/Lifecycle.vue
@@ -1,58 +1,35 @@
 <template>
-  <div ref="refDiv">{{ text }}</div>
-  <button @click="change">改字</button>
-
-  <div>{{ person.name }}</div>
-  <div>{{ person.num }}</div>
-  <div>computed: {{ rename }}</div>
-
-  <button @click="changeName">改名</button>
-  <button @click="() => person.num++">++</button>
+  <input
+    v-for="item in colorList"
+    :key="item.name"
+    type="radio"
+    v-model="color"
+    :value="item.color"
+  />
+  <ul>
+    <li class="color">123</li>
+  </ul>
 </template>
 
 <script setup>
-import { ref, reactive, watch, computed } from 'vue'
+import { ref, reactive } from 'vue'
 
-const refDiv = ref(null)
-const text = ref('div')
-const person = reactive({
-  name: 'KT',
-  num: 1
-})
-
-const change = () => {
-  console.log('click change')
-  text.value = 'new div'
-}
-
-const changeName = () => {
-  console.log('click changeName')
-  person.name = 'KT2'
-}
-
-const rename = computed(() => person.name + person.num)
-
-watch(
-  () => text.value,
-  (nv, ov) => {
-    console.log('watch text nv', nv)
-    console.log('watch text ov', ov)
+const colorList = reactive([
+  {
+    name: 'red',
+    color: 'red'
+  },
+  {
+    name: 'blue',
+    color: 'blue'
   }
-)
+])
 
-watch(
-  () => ({ ...person }),
-  (nv, ov) => {
-    console.log('watch person nv', nv)
-    console.log('watch person ov', ov)
-  }
-)
-
-watch(
-  () => rename.value,
-  (nv, ov) => {
-    console.log('watch rename nv', nv)
-    console.log('watch rename ov', ov)
-  }
-)
+const color = ref(colorList[0].color)
 </script>
+
+<style>
+.color {
+  color: v-bind('color');
+}
+</style>
